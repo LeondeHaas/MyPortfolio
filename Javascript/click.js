47530d2874f1b8402811cddb91f5bf7886ced788
Leon de Haas <96114373+LeondeHaas@users.noreply.github.com>
2023-04-16 00:59:19 +0200

    more sounds, ton of styling doneg

--- Javascript/click.js
+++ Javascript/click.js
@@ -2,4 +2,7 @@
 // audio.src = "./sounds/mixkit-clear-mouse-clicks-2997.wav"
 
 const audio = new Audio();
-audio.src = "./sounds/jixaw-metal-pipe-falling-sound.mp3"
+audio.src = "./sounds/mixkit-hard-typewriter-click-1119.wav"
+
+// const audio = new Audio();
+// audio.src = "./Sounds/jixaw-metal-pipe-falling-sound.mp3"
