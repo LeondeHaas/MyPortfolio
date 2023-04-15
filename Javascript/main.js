8c4b5b9fa01479567e0b8f496fd3ca5ad27656ff
Leon de Haas <96114373+LeondeHaas@users.noreply.github.com>
2023-04-15 16:48:47 +0200

    more responsive, more styling added back to top button

--- Javascript/main.js
+++ Javascript/main.js
@@ -10,30 +10,3 @@ function clickEffect(e) {
 document.addEventListener('click', clickEffect);
 
 // animation scroll
-
-// Create the observer
-const observer = new IntersectionObserver(entries => {
-    // We will fill in the callback later...
-});
-
-// Tell the observer which elements to track
-observer.observe(document.querySelector('.skill-card'));
-
-entries.forEach(entry => {
-    if (entry.isIntersecting) {
-        // It's visible. Add the animation class here!
-    }
-});
-
-const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-        // If the element is visible
-        if (entry.isIntersecting) {
-            // Add the animation class
-            entry.target.classList.add('square-animation');
-        }
-    });
-});
-
-observer.observe(document.querySelector('.skill-card'));
---
+++ Javascript/text.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $("#fold").click(function () {
+        $("#fold_p").text("Are you ok?");
+    })
+});
